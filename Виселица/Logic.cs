e8d1a4aea17300b7959dc24dae0f5d610bf3a807
Савелий <savelussava@mail.ru>
2021-01-29 12:05:53 +0300

    Clear code + added check for entering lives

--- Виселица/Logic.cs
+++ Виселица/Logic.cs
@@ -17,27 +17,31 @@ namespace Виселица
         public void Play(int counterLifes, WorkInConsole consoleworker)
         {
             string word = Words.GiveOutWord();
-            int lengthword = word.Length;
-            int remainingCharacters = lengthword;
-            char[] encryptedWord = new char[lengthword];
-            for (int i = 0; i < lengthword; i++)
-            {
-                encryptedWord[i] = '_';
-            }
+            int lengthWord = word.Length;
+            int remainingCharacters = lengthWord;
+            char[] encryptedWord = GetEncryptedWord(lengthWord);
             while (remainingCharacters != 0 && counterLifes != 0 )
             {
                 consoleworker.DisplayWordwithLifes(counterLifes, new string(encryptedWord));
                 char sym = consoleworker.ReadSymbol();
-                counterLifes = UpdateStats
-                    (ref remainingCharacters, counterLifes, sym, word, ref encryptedWord);
+                UpdateStats(ref remainingCharacters, ref counterLifes, sym, word, encryptedWord);
             }
-            consoleworker.End();
+            consoleworker.WriteEndMessage();
         }
-        public int UpdateStats(ref int remainingCharacters, int counterLifes, char sym, string word, ref char[] encryptedWord)
+        private char[] GetEncryptedWord(int lengthWord)
+        {
+            char[] encryptedWord = new char[lengthWord];
+            for (int i = 0; i < lengthWord; i++)
+            {
+                encryptedWord[i] = '_';
+            }
+            return encryptedWord;
+        }
+        private void UpdateStats(ref int remainingCharacters, ref int counterLifes, char sym, string word,  char[] encryptedWord)
         {
             if (!IsContains(word, sym))
             {
-                return counterLifes -= 1;
+                counterLifes -= 1;
             }
             else
             {
@@ -49,21 +53,19 @@ namespace Виселица
                         remainingCharacters -= 1;
                     }
                 }
-                return counterLifes;
             }
         }
-        public bool IsContains(string word, char sym)
+        private bool IsContains(string word, char sym)
         {
-            bool isContain = false;
             foreach (var ch in word)
             {
                 if (ch == sym)
                 {
-                    isContain = true;
-                    break;
+                    return true;
                 }
             }
-            return isContain;
+            return false;
+            
         }
     }
 }
--- Виселица/WorkInConsole.cs
+++ Виселица/WorkInConsole.cs
@@ -10,9 +10,21 @@ namespace Виселица
     {
         public int WriteStartMessage()
         {
-            Console.WriteLine("Здравсвтуй, сейчас я загадаю слово, выбери сколько жизней у тебя будет");
-            int lifes = int.Parse(Console.ReadLine());
+            int lifes = 1;
+            Console.WriteLine("Здравсвтуй, сейчас я загадаю слово. Выбери сколько жизней у тебя будет");
+            bool resultParse = int.TryParse(Console.ReadLine(), out lifes)
+            if(!resultParse)
+            {
+                do
+                {
+                    Console.WriteLine("Вы ввели что-то не так, попробуйте еще раз");
+                    resultParse = int.TryParse(Console.ReadLine(), out lifes);
+                }
+                while (!resultParse);
+            }
             return lifes;
+           
+
         }
         public void DisplayWordwithLifes(int lifes, string encryptedWord)
         {
@@ -27,7 +39,7 @@ namespace Виселица
             char[] inputinchar = input.ToLower().ToCharArray();
             return inputinchar[0];
         }
-        public void End()
+        public void WriteEndMessage()
         {
             Console.Clear();
             Console.WriteLine("Конец игры друг");
